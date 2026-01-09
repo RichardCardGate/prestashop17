e3421bd789c190573be863d4437d261f8bf89223
richard <richard@cardgate.com>
2026-01-09 14:42:01 +0100

    Version bump.

--- modules/cardgate/cardgate.php
+++ modules/cardgate/cardgate.php
@@ -26,7 +26,7 @@ class Cardgate extends CardgatePayment
 
     public function __construct()
     {
-        Configuration::updateValue('CARDGATE_MODULE_VERSION', '1.7.19');
+        Configuration::updateValue( 'CARDGATE_MODULE_VERSION', '1.7.20' );
         $this->name = 'cardgate';
         $this->paymentcode = 'cardgate';
         $this->paymentname = 'CardGate';
